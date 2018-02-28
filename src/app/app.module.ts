f462f0ecf608c09bd84891de63ff7ee4b944ed4e
Michael De Abreu <michaeljota@outlook.com>
2018-02-28 12:29:27 -0400

    feat: create layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { LayoutComponent } from './layout/layout.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LayoutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
