6b6218a940ca865c3d4ee43551fc03a2e77520fd
Michael De Abreu <michaeljota@outlook.com>
2018-02-28 11:14:28 -0400

    feat(heroes): add heroes module

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+@NgModule({
+  imports: [CommonModule, HeroesRoutingModule],
+  declarations: [],
+})
+export class HeroesModule {}
