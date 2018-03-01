bc70293b46d90ce9a9e116c844e952f4565ad651
Michael De Abreu <michaeljota@outlook.com>
2018-03-01 10:28:47 -0400

    feat(heroes): add material module to heroes feat module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HeroesMaterialModule } from './heroes-material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 
 @NgModule({
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, HeroesMaterialModule, HeroesRoutingModule],
   declarations: [],
 })
 export class HeroesModule {}
