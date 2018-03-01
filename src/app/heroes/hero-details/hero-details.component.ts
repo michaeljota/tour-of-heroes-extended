3cc8ef8da44faace8499420f64cac3387a9954ea
Michael De Abreu <michaeljota@outlook.com>
2018-03-01 18:14:21 -0400

    feat(hero-details): hero details component read hero from route param

--- src/app/heroes/hero-details/hero-details.component.ts
+++ src/app/heroes/hero-details/hero-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { pluck, mergeMap } from 'rxjs/operators';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-details',
@@ -6,7 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-details.component.scss'],
 })
 export class HeroDetailsComponent implements OnInit {
-  constructor() {}
+  public hero: Observable<Hero>;
 
-  ngOnInit() {}
+  constructor(
+    private readonly heroService: HeroService,
+    private readonly route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.hero = this.route.params.pipe(
+      pluck('id'),
+      mergeMap((id: string) => this.heroService.get(id)),
+    );
+  }
 }
