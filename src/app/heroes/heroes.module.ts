eb985a04915e9ef499cec9dd576a99af1f233f3e
Michael De Abreu <michaeljota@outlook.com>
2018-03-01 18:18:22 -0400

    feat(heroes): heroes module import reactive forms module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeroesMaterialModule } from './heroes-material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
@@ -10,7 +11,12 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
-  imports: [CommonModule, HeroesMaterialModule, HeroesRoutingModule],
+  imports: [
+    CommonModule,
+    HeroesMaterialModule,
+    HeroesRoutingModule,
+    ReactiveFormsModule,
+  ],
   declarations: [HeroDetailsComponent, HeroesComponent],
   providers: [HeroService],
 })
