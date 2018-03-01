3a3e215caa135842aca367b69586d9c044651b4f
Michael De Abreu <michaeljota@outlook.com>
2018-03-01 16:12:55 -0400

    feat(heroes): create hero details component

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HeroesMaterialModule } from './heroes-material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 import { HeroesComponent } from './heroes/heroes.component';
+
 import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [CommonModule, HeroesMaterialModule, HeroesRoutingModule],
-  declarations: [HeroesComponent],
+  declarations: [HeroDetailsComponent, HeroesComponent],
   providers: [HeroService],
 })
 export class HeroesModule {}
