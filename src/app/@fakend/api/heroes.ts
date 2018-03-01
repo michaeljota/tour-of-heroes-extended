bdd41b6bb5387c02c6c763dd96a470a87be69be9
Michael De Abreu <michaeljota@outlook.com>
2018-03-01 12:22:07 -0400

    feat(fakend): add heroes endpoint data

---
+++ src/app/@fakend/api/heroes.ts
@@ -0,0 +1,206 @@
+/*
+ * Generated with https://random-hero-generator.stackblitz.io/
+ */
+
+export const heroes = [
+  {
+    id: 'ddd287d0-f132-4c76-87d3-83448f990985',
+    name: 'Zinceagle Ninja',
+  },
+  {
+    id: 'ac26b690-713e-44f4-b10e-92d2ac2973ed',
+    name: 'Pinemouse Grabber',
+  },
+  {
+    id: '0fc21f65-646a-4d55-bef9-ae9169049d1f',
+    name: 'Basalthawk Flier',
+  },
+  {
+    id: '583fd86d-0a7f-4bcd-962d-ca5b1be55cff',
+    name: 'Honeysucklegambler Legend',
+  },
+  {
+    id: 'bfea407a-6d67-4ab5-a4a0-4b166a2822a2',
+    name: 'Battlehorn Legend',
+  },
+  {
+    id: 'd23b0e75-c941-4ce1-beea-7ee44c34f860',
+    name: 'Embersong Grin',
+  },
+  {
+    id: '0b560027-9026-436a-952a-4a70390b7a82',
+    name: 'Mountaindolphin Swoop',
+  },
+  {
+    id: '51ab07ec-e635-4d77-af39-c795a39769bc',
+    name: 'Snapdragonfoot Snap',
+  },
+  {
+    id: '6ff09834-06cf-44d1-95aa-779eab2a34ad',
+    name: 'Coffeeiguana Crafter',
+  },
+  {
+    id: 'a51668e5-d733-4fc2-8a51-0eec652302bf',
+    name: 'Noblespur Snapper',
+  },
+  {
+    id: '97fcde08-f3cb-44cb-a8d2-d90cdfee80ca',
+    name: 'Pepperbuffalo Biter',
+  },
+  {
+    id: '9d100128-a575-4588-9818-bea38083dd4e',
+    name: 'Rattlehiss Fang',
+  },
+  {
+    id: 'eae15ab1-9c24-4901-9fd2-7448c0f5fa6f',
+    name: 'Ragdancer Cloud',
+  },
+  {
+    id: '9dc4d935-5727-498c-afc6-5d8c7f621ec1',
+    name: 'Pintobane Toucan',
+  },
+  {
+    id: '8b61fd05-2cf7-42e8-9ef7-efe6d6547be8',
+    name: 'Frostweasel Pixie',
+  },
+  {
+    id: '6d6b8332-9692-4107-9509-c2a4c32f558a',
+    name: 'Chiselyak Hisser',
+  },
+  {
+    id: 'cd4d0d3d-93a0-4c06-9bda-2035c230a3ad',
+    name: 'Twilightdiver Dog',
+  },
+  {
+    id: '8757ded4-e711-44fe-8a47-763ac0d721e1',
+    name: 'Springgazelle Raccoon',
+  },
+  {
+    id: '97d3b09b-0205-4ed4-b69e-78e38f4ab5df',
+    name: 'Maplespeaker Forger',
+  },
+  {
+    id: 'e71c81ab-1b7f-46ed-b212-2033629a4808',
+    name: 'Vividspear Heron',
+  },
+  {
+    id: '475792b0-ba47-47b7-8d00-28e98039e771',
+    name: 'Autumnspear Fang',
+  },
+  {
+    id: '3c382c70-f575-4ccd-97ba-29e7cc0bcd5b',
+    name: 'Slimeringer Paw',
+  },
+  {
+    id: 'b5f234d3-754d-45da-a6b4-2db7973b150b',
+    name: 'Stitchcarpet Antelope',
+  },
+  {
+    id: '055c0c60-5460-4a2b-bfa7-d553badd5321',
+    name: 'Polarmind Catcher',
+  },
+  {
+    id: 'c941489e-d831-4c73-8c2c-26e4be74cd8e',
+    name: 'Patternscarer Snarl',
+  },
+  {
+    id: '1f2f15a2-70c8-45a2-945c-f1f1ea53232b',
+    name: 'Meadowhead Face',
+  },
+  {
+    id: '8306e41c-9f7f-429c-ab41-3cfecff4a564',
+    name: 'Quilttwister Bone',
+  },
+  {
+    id: '679844d3-9ffc-4a73-9986-dd8e6d94d54d',
+    name: 'Magentapaw Knife',
+  },
+  {
+    id: 'fc11d508-dc73-45d5-9959-83f6c53e378e',
+    name: 'Carnationhorn Carpet',
+  },
+  {
+    id: 'a6a9f384-0e01-412f-99f7-ad9ba56cea4c',
+    name: 'Boldwing Roarer',
+  },
+  {
+    id: '1b2d5ad0-5ce4-4a02-a29e-aeb2659566d7',
+    name: 'Steelox Twister',
+  },
+  {
+    id: '8609fbd6-5520-48a2-bb6c-46cea12041c4',
+    name: 'Spotpiper Player',
+  },
+  {
+    id: '03e85dff-3006-466d-b574-0de1b7c236e4',
+    name: 'Quivertooth Crow',
+  },
+  {
+    id: '95e7e22b-f41e-485c-ae2b-13b9ec60a588',
+    name: 'Harvesthugger Scale',
+  },
+  {
+    id: 'a98c2081-638e-4da3-9588-8d6bf4b7ea16',
+    name: 'Olivesnarl Fang',
+  },
+  {
+    id: 'bcf73cae-0a74-4b1d-a125-3575165202f4',
+    name: 'Cutedoom Fish',
+  },
+  {
+    id: 'af836507-1050-40a4-9556-87b335238a1d',
+    name: 'Splitcarp Hawk',
+  },
+  {
+    id: 'a355f93b-c90b-4dc6-8ac1-410cba7e169a',
+    name: 'Scarletcondor Chin',
+  },
+  {
+    id: '4b0ea466-b087-4b4a-8097-dbbb9790a2c8',
+    name: 'Dirtsting Drifter',
+  },
+  {
+    id: '8670deb7-3153-4e95-99c2-81b3adbb39f7',
+    name: 'Softbrow Hyena',
+  },
+  {
+    id: '8aa484a7-77ea-4451-88bc-1cb1b74fee1e',
+    name: 'Rubyotter Touch',
+  },
+  {
+    id: '229b730a-f32f-4a0d-b6c3-6d847284a728',
+    name: 'Specklesalmon Carpet',
+  },
+  {
+    id: '8359f78a-d3f5-4cb3-aa7a-9d8ee89ec3f5',
+    name: 'Savageedge Buffalo',
+  },
+  {
+    id: '977fc34f-b652-4fe5-8aaf-df0a4235c1c3',
+    name: 'Tundrahisser Wolf',
+  },
+  {
+    id: 'c1087c38-551f-46ef-b874-3591dd00c20c',
+    name: 'Rainbowtouch Goose',
+  },
+  {
+    id: '201f41db-5d00-4ae0-9dd9-cf4307246bb8',
+    name: 'Zenithhead Otter',
+  },
+  {
+    id: '3a5e31a1-aeb2-409c-ac94-3360840be7c8',
+    name: 'Dirtbane Pig',
+  },
+  {
+    id: '7ebf8a11-a5de-410b-b7a0-a7b09d47033c',
+    name: 'Gossamercondor Tail',
+  },
+  {
+    id: 'f926ac43-f262-48cc-a9f3-241a72c3aad9',
+    name: 'Valiantsnapper Centaur',
+  },
+  {
+    id: 'cef4491d-c092-4949-918b-d66d9247e210',
+    name: 'Hickorysoarer Crafter',
+  },
+];
