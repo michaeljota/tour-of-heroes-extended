8683f7ec410e1ec647a18bc9144910830cfaa70c
Michael De Abreu <michaeljota@outlook.com>
2018-03-01 13:28:07 -0400

    feat(heroes): add hero service to call heroes endpoint

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,8 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HeroesMaterialModule } from './heroes-material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 
+import { HeroService } from './hero.service';
+
 @NgModule({
   imports: [CommonModule, HeroesMaterialModule, HeroesRoutingModule],
   declarations: [],
+  providers: [HeroService],
 })
 export class HeroesModule {}
