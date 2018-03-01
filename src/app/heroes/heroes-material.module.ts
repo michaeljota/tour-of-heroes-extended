3e858504b2d333efb14db8980c5c8f09f8f2f278
Michael De Abreu <michaeljota@outlook.com>
2018-03-01 16:02:52 -0400

    feat(heroes): import mat list module into heroes material module

--- src/app/heroes/heroes-material.module.ts
+++ src/app/heroes/heroes-material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material';
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [MatListModule],
+  exports: [MatListModule],
 })
 export class HeroesMaterialModule {}
