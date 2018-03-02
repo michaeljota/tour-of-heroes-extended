d9e303967bc26ef1407ff52b67080485c46dc9b7
Michael De Abreu <michaeljota@outlook.com>
2018-03-02 17:12:20 -0400

    feat(heroes): heroes component use ngrx store

--- src/app/heroes/heroes/heroes.component.ts
+++ src/app/heroes/heroes/heroes.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import { startWith } from 'rxjs/operators';
 
-import { HeroService } from '../hero.service';
+import { AppState } from '../../app-store.module';
+
 import { Hero } from '../hero';
+
+import { HeroesActions } from './heroes.actions';
+import { selectHeroesList } from './heroes.selectors';
 
 @Component({
   selector: 'app-heroes',
@@ -10,11 +16,15 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  public heroes: Observable<Hero[]>;
+  public heroes: Observable<ReadonlyArray<Hero>>;
 
-  constructor(private readonly heroService: HeroService) {}
+  constructor(
+    private readonly store: Store<AppState>,
+    private readonly actions: HeroesActions,
+  ) {}
 
   ngOnInit() {
-    this.heroes = this.heroService.getAll();
+    this.store.dispatch(this.actions.load());
+    this.heroes = this.store.select(selectHeroesList).pipe(startWith([]));
   }
 }
